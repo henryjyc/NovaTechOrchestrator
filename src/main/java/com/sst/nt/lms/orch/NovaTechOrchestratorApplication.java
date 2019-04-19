3c17878c252f53012111f2f991d0397bc51112ae
Jonathan Lovelace <kingjon3377@gmail.com>
2019-04-19 13:03:20 -0400

    Initialize as Eclipse project.

---
+++ src/main/java/com/sst/nt/lms/orch/NovaTechOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package com.sst.nt.lms.orch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaTechOrchestratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovaTechOrchestratorApplication.class, args);
+	}
+
+}
