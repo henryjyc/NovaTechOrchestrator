f0793ad838ed10867b12796e8c77deaf3b2a6c41
Salem Ozaki <SOzaki19@cornellcollege.edu>
2019-04-19 18:18:12 -0400

    add restTemplate and Load Balenced to main

--- src/main/java/com/sst/nt/lms/orch/NovaTechOrchestratorApplication.java
+++ src/main/java/com/sst/nt/lms/orch/NovaTechOrchestratorApplication.java
@@ -2,6 +2,9 @@ package com.sst.nt.lms.orch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class NovaTechOrchestratorApplication {
@@ -10,4 +13,7 @@ public class NovaTechOrchestratorApplication {
 		SpringApplication.run(NovaTechOrchestratorApplication.class, args);
 	}
 
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() { return new RestTemplate();}
 }
